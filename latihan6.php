f6cfaa0071e438a960ad8fff74a67dda8f7459e8
fathanf23 <143672273+fathanf23@users.noreply.github.com>
2023-09-25 18:38:19 +0700

    Update latihan6.php
    harusnya 74 bukan 7 salah hehe

--- latihan6.php
+++ latihan6.php
@@ -34,7 +34,7 @@ $tombol = $_POST['proses'];
 $ket = ($nilai >= 60) ? "Lulus" : "Gagal";
 if($nilai >= 85 && $nilai <= 100) $grade = "A";
 else if($nilai >= 75 && $nilai <= 84) $grade = "B";
-else if($nilai >= 60 && $nilai <= 7) $grade = "C";
+else if($nilai >= 60 && $nilai <= 74) $grade = "C";
 else if($nilai >= 30 && $nilai <= 59) $grade = "D";
 else if($nilai >= 0 && $nilai <= 29) $grade = "E";
 else $grade = "";
